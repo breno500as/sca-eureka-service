bdd59b6ef1f72ce88d5512fec2dc499c12db06e8
breno.mcosta <breno.mcosta@pbh.gov.br>
2019-07-04 09:45:26 -0300

    configurações iniciais

---
+++ src/main/java/com/puc/sca/eureka/service/ScaEurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.puc.sca.eureka.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaEurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScaEurekaServiceApplication.class, args);
+	}
+
+}
