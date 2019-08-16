a85aa1ab45338353a4118d05c2b7334e60dce9b2
breno.mcosta <breno.mcosta@pbh.gov.br>
2019-08-16 10:25:52 -0300

    documentação

--- src/main/java/com/puc/sca/eureka/service/ScaEurekaServiceApplication.java
+++ src/main/java/com/puc/sca/eureka/service/ScaEurekaServiceApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Aplicação que orquestra e monitora os microserviços.
+ * @author breno
+ *
+ */
+
 @SpringBootApplication
 @EnableEurekaServer
 public class ScaEurekaServiceApplication {
